bf83f244a2c3c1f446ce1c7bd5b86539354d0149
DevNico2905 <nbernal36@unisalle.edu.co>
2026-08-15 19:05:33 -0500

    Task Service and Impl

---
+++ src/main/java/com/devtiro/task/service/impl/TaskServiceImpl.java
@@ -0,0 +1,15 @@
+package com.devtiro.task.service.impl;
+import com.devtiro.task.domain.CreateTaskRequest;
+import com.devtiro.task.domain.entity.Task;
+import com.devtiro.task.service.TaskService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+
+    @Override
+    public Task createTask(CreateTaskRequest request) {
+        return null;
+    }
+}
