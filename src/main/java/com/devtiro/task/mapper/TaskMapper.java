f7e72f28ad3e3e5a41af1117c77d0af561705388
DevNico2905 <nbernal36@unisalle.edu.co>
2026-08-17 18:03:47 -0500

    Uptade Mapper

--- src/main/java/com/devtiro/task/mapper/TaskMapper.java
+++ src/main/java/com/devtiro/task/mapper/TaskMapper.java
@@ -1,8 +1,10 @@
 package com.devtiro.task.mapper;
 
 import com.devtiro.task.domain.CreateTaskRequest;
+import com.devtiro.task.domain.UpdateTaskRequest;
 import com.devtiro.task.domain.dto.CreateTaskRequestDto;
 import com.devtiro.task.domain.dto.TaskDto;
+import com.devtiro.task.domain.dto.UpdateTaskRequestDto;
 import com.devtiro.task.domain.entity.Task;
 
 public interface TaskMapper {
@@ -10,4 +12,6 @@ public interface TaskMapper {
     CreateTaskRequest fromDto(CreateTaskRequestDto dto);
 
     TaskDto toDto(Task task);
+
+    UpdateTaskRequest fromDto(UpdateTaskRequestDto dto);
 }
--- src/main/java/com/devtiro/task/mapper/impl/TaskMapperImpl.java
+++ src/main/java/com/devtiro/task/mapper/impl/TaskMapperImpl.java
@@ -1,7 +1,9 @@
 package com.devtiro.task.mapper.impl;
 import com.devtiro.task.domain.CreateTaskRequest;
+import com.devtiro.task.domain.UpdateTaskRequest;
 import com.devtiro.task.domain.dto.CreateTaskRequestDto;
 import com.devtiro.task.domain.dto.TaskDto;
+import com.devtiro.task.domain.dto.UpdateTaskRequestDto;
 import com.devtiro.task.domain.entity.Task;
 import com.devtiro.task.mapper.TaskMapper;
 import org.springframework.stereotype.Component;
@@ -27,6 +29,17 @@ public class TaskMapperImpl implements TaskMapper {
                 task.getDueDate(),
                 task.getPriority(),
                 task.getStatus()
+        );
+    }
+
+    @Override
+    public UpdateTaskRequest fromDto(UpdateTaskRequestDto dto) {
+        return new UpdateTaskRequest(
+                dto.title(),
+                dto.description(),
+                dto.dueDate(),
+                dto.status(),
+                dto.priority()
         );
     }
 }
